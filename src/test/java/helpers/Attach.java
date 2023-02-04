c26aed56232b94cf0e0d09f43c30f387bd9a5fff
Marina Konovalchik <marina200014@yandex.by>
2023-02-05 02:04:46 +0300

    updated files

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -15,6 +15,7 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.openqa.selenium.logging.LogType.BROWSER;
 
 public class Attach {
+
         @Attachment(value = "{attachName}", type = "image/png")
         public static byte[] screenshotAs(String attachName) {
             return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
@@ -54,5 +55,5 @@ public class Attach {
             }
             return null;
         }
-}
+    }
 
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -10,19 +10,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
-        @BeforeAll
-        static void beforeAll() {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.timeout = 10000;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
-            Configuration.baseUrl = "https://demoqa.com";
-            Configuration.browserSize = "1920x1080";
-            Configuration.timeout = 10000;
-            Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("enableVNC", true);
-            capabilities.setCapability("enableVideo", true);
-            Configuration.browserCapabilities = capabilities;
-        }
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
 
     @BeforeEach
     void addListener() {
@@ -36,6 +35,6 @@ public class TestBase {
         Attach.browserConsoleLogs();
         Attach.addVideo();
     }
-
 }
+
 
